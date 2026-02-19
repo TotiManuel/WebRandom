f6824997536477b4a0e15060cd3951f999a94dc4
TotiManuel <manuel.mandaio@gmail.com>
2026-02-19 00:17:24 -0300

    New Gift

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,14 @@
-self.addEventListener("install", e=>{
-    self.skipWaiting();
+const CACHE_NAME = "mensaje-cache-v1";
+const urlsToCache = ["/", "/static/images/icon.png", "/home"];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
